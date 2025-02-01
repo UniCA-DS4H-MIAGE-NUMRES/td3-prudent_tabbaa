4b001966340db1f7ef3f5f581962e19784ee8bf0
stabbaa <siham.tabbaa@etu.unice.fr>
2025-02-01 20:51:15 +0100

    Delete all folder  :| :'(

--- composeApp/src/commonMain/kotlin/fr/unica/miage/tabbaa/pizzapp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package fr.unica.miage.tabbaa.pizzapp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/desktopMain/kotlin/fr/unica/miage/tabbaa/pizzapp/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package fr.unica.miage.tabbaa.pizzapp
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
