beb1613acd001b93a78e2d1fb29548710a075572
Gautier Benoit <https://gitlab.com/gautier_bnt>
2025-02-09 14:40:07 +0100

    web independances

--- composeApp/src/wasmJsMain/kotlin/fr/unica/miage/tabbaa/pizzapp/data/OrderRepository.kt
+++ composeApp/src/wasmJsMain/kotlin/fr/unica/miage/tabbaa/pizzapp/data/OrderRepository.kt
@@ -1,4 +1,23 @@
 package fr.unica.miage.tabbaa.pizzapp.data
 
-class OrderRepository {
+import fr.unica.miage.tabbaa.pizzapp.model.Order
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+actual class OrderRepository actual constructor(private val dao: OrderDao) {
+    actual fun getOrders(): Flow<List<Order>> {
+        return dao.getAllOrders()
+    }
+
+    actual suspend fun addOrder(order: Order) {
+
+    }
+
+    actual suspend fun deleteAllOrders() {
+
+    }
+
 }
--- composeApp/src/wasmJsMain/kotlin/fr/unica/miage/tabbaa/pizzapp/utils/ImageLoader.kt
+++ composeApp/src/wasmJsMain/kotlin/fr/unica/miage/tabbaa/pizzapp/utils/ImageLoader.kt
@@ -2,9 +2,14 @@ package fr.unica.miage.tabbaa.pizzapp.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.InternalResourceApi
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalResourceApi::class, InternalResourceApi::class)
 @Composable
 actual fun loadImage(resourcePath: String): Painter {
-    return painterResource(resourcePath)
+    val drawable = DrawableResource("drawable/$resourcePath", emptySet())
+    return painterResource(drawable)
 }
